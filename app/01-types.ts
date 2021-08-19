320afec52bb30fbf2b247852968410a3141dcf44
Brad Titus <bltitus@me.com>
2021-08-18 22:11:51 -0400

    Module 2.03

--- app/01-types.ts
+++ app/01-types.ts
@@ -11,3 +11,38 @@ let isMarried: boolean;
 let greeting = "Hello";
 
 password2 = "1234";
+
+let age: number = 40;
+let currentYear: number = 2021;
+
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+
+let companyName: string;
+companyName = "Eleven Fifty";
+
+let appName: string = 'Twitter';
+appName = "Instagram";
+
+let isLoggedIn: boolean = true;
+let isGoldLevelUser: boolean = true;
+
+let studentList: string[] = ['Tom', 'Tracy', 'Nikaya'];
+
+let allScores: Array<number> = [100, 92, 95, 96];
+
+console.log("Student List:", studentList[0]);
+
+let dataFromThirdParty: any = '12345';
+console.log(dataFromThirdParty);
+console.log(typeof dataFromThirdParty);
+
+dataFromThirdParty = 12345;
+console.log(dataFromThirdParty);
+console.log(typeof dataFromThirdParty);
+
+function sayHelloToAll() : void {
+    console.log("Hello to all!")
+}
+
--- js/01-types.js
+++ js/01-types.js
@@ -8,4 +8,27 @@ var socialSecurity;
 var isMarried;
 var greeting = "Hello";
 password2 = "1234";
+var age = 40;
+var currentYear = 2021;
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+var companyName;
+companyName = "Eleven Fifty";
+var appName = 'Twitter';
+appName = "Instagram";
+var isLoggedIn = true;
+var isGoldLevelUser = true;
+var studentList = ['Tom', 'Tracy', 'Nikaya'];
+var allScores = [100, 92, 95, 96];
+console.log("Student List:", studentList[0]);
+var dataFromThirdParty = '12345';
+console.log(dataFromThirdParty);
+console.log(typeof dataFromThirdParty);
+dataFromThirdParty = 12345;
+console.log(dataFromThirdParty);
+console.log(typeof dataFromThirdParty);
+function sayHelloToAll() {
+    console.log("Hello to all!");
+}
 //# sourceMappingURL=01-types.js.map
