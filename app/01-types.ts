03fcc5d2a279c6e1192d51ac4c42ffd0273a126f
Brad Titus <bltitus@me.com>
2021-08-18 21:55:00 -0400

    Module 2.02

---
+++ app/01-types.ts
@@ -0,0 +1,13 @@
+let username: string = "instructor";
+let password: string = "Letmein1234!";
+let instructorId: number = 5;
+
+let password2: string;
+let phoneNumber: number;
+let email: string;
+let socialSecurity: number;
+let isMarried: boolean;
+
+let greeting = "Hello";
+
+password2 = "1234";
---
+++ js/01-types.js
@@ -0,0 +1,11 @@
+var username = "instructor";
+var password = "Letmein1234!";
+var instructorId = 5;
+var password2;
+var phoneNumber;
+var email;
+var socialSecurity;
+var isMarried;
+var greeting = "Hello";
+password2 = "1234";
+//# sourceMappingURL=01-types.js.map
