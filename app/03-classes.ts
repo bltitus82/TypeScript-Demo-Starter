f53771e499ddc8b0472dab30111f6525aaf017ec
Brad Titus <bltitus@me.com>
2021-08-19 18:46:01 -0400

    Module 2.09

--- app/03-classes.ts
+++ app/03-classes.ts
@@ -1,6 +1,14 @@
 class Person {
     firstName: string;
     lastName: string;
+
+    sayHello(){
+        console.log("Hello", this.firstName);
+    }
+
+    sayHelloToFullName(){
+        console.log("Hello", this.firstName, this.lastName);
+    }
 }
 
 let phil: Person = new Person();
@@ -9,3 +17,16 @@ phil.firstName = "Phil";
 phil.lastName = "Donahue";
 
 let someVariableName: Person = new Person();
+
+let kenn: Person = new Person();
+
+kenn.firstName = "Kenn";
+
+kenn.sayHello();
+
+let sheriff: Person = new Person();
+
+sheriff.firstName = "Sheriff";
+sheriff.lastName = "Holler";
+
+sheriff.sayHelloToFullName();
--- js/03-classes.js
+++ js/03-classes.js
@@ -1,10 +1,23 @@
 var Person = /** @class */ (function () {
     function Person() {
     }
+    Person.prototype.sayHello = function () {
+        console.log("Hello", this.firstName);
+    };
+    Person.prototype.sayHelloToFullName = function () {
+        console.log("Hello", this.firstName, this.lastName);
+    };
     return Person;
 }());
 var phil = new Person();
 phil.firstName = "Phil";
 phil.lastName = "Donahue";
 var someVariableName = new Person();
+var kenn = new Person();
+kenn.firstName = "Kenn";
+kenn.sayHello();
+var sheriff = new Person();
+sheriff.firstName = "Sheriff";
+sheriff.lastName = "Holler";
+sheriff.sayHelloToFullName();
 //# sourceMappingURL=03-classes.js.map
