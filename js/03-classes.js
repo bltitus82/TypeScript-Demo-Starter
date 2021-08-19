3cf251f6bbc5af82c0075d7f65686bd384a39330
Brad Titus <bltitus@me.com>
2021-08-19 18:23:07 -0400

    Module 2.08

--- js/03-classes.js
+++ js/03-classes.js
@@ -1,1 +1,10 @@
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    return Person;
+}());
+var phil = new Person();
+phil.firstName = "Phil";
+phil.lastName = "Donahue";
+var someVariableName = new Person();
 //# sourceMappingURL=03-classes.js.map
