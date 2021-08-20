586116187315b8bcf9d6f015ff0d61ba721343dd
Brad Titus <bltitus@me.com>
2021-08-19 20:38:21 -0400

    Module 2.15

--- app/04-interfaces.ts
+++ app/04-interfaces.ts
@@ -7,3 +7,31 @@ let lassie: Animal = {
     name: 'Lassie',
     numberOfLegs: 4
 }
+
+
+
+
+
+// sayHello(name: string) : string {
+//     var someNum = 1 + 2;
+//     var someOtherNum = 1 + 2;
+//     return name;
+// }
+
+interface Product {
+    name: string;
+    price: number;
+    setTimeOfPurchase(d: Date);
+}
+
+class Apples implements Product {
+    name: 'Apples';
+    price: 5.50;
+
+    setTimeOfPurchase(d: Date) {
+        console.log("Apples purchased on ", d);
+    }
+}
+
+let myApples: Apples = new Apples();
+myApples.setTimeOfPurchase(new Date(Date.now()));
